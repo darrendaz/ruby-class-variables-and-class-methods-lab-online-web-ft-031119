0609fb7f15132dcde90444a444e76e8d95aa4974
Darren L <darren.lou@gmail.com>
2019-03-24 00:22:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,8 @@ class Song
   def genre_count
     hash = {}
     @@genres.uniq.each {|genre|
-      hash = {"#{genre}" = > @@genres.count(genre)}
+      hash = {"#{genre}" = @@genres.count(genre)}
     }
-    
-#     ary = [1, 2, 4, 2]
-#     ary.count             #=> 4
-#     ary.count(2)          #=> 2
-#     ary.count{|x|x%2==0}  #=> 3
     
   end
   
